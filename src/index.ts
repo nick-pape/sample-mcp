390aa1ba0c4693204b2b4a2aa77f6241ae97f4e6
Nick Pape <5674316+nick-pape@users.noreply.github.com>
2025-06-19 01:17:39 -0500

    Example tool implementation

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,41 @@
-export const FOO: string = 'foo';
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio";
+import { z } from "zod";
+
+const server: McpServer = new McpServer({
+  name: "test-server",
+  version: "0.0.1",
+  capabilities: {
+    resources: {},
+    tools: {}
+  }
+});
+
+server.tool(
+  "get-phone-number",
+  "Gets the phone number of a user or person, using their name.",
+  {
+    name: z.string().describe("The name of the person to get the phone number for.")
+  },
+  async ({ name: string }) => {
+    return {
+      content: [
+        {
+          type: "text",
+          text: `123-456-7890`
+        }
+      ]
+    }
+  }
+);
+
+async function main(): Promise<void> {
+  const transport: StdioServerTransport = new StdioServerTransport();
+  await server.connect(transport);
+  console.log("Server is running and listening for requests...");
+}
+
+main().catch((error) => {
+  console.error("Error starting server:", error);
+  process.exit(1);
+});
