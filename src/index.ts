98274cf8faa006e68ad4646d7d3985428d1ecba7
Nick Pape <5674316+nick-pape@users.noreply.github.com>
2025-06-20 10:29:36 -0500

    Add the MCP->Tool server proxy

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { z } from "zod";
+import { FileSystem, FolderItem } from "@rushstack/node-core-library";
 
 const server: McpServer = new McpServer({
   name: "test-server",
@@ -23,6 +24,59 @@ server.tool(
         {
           type: "text",
           text: `123-456-7890`
+        }
+      ]
+    }
+  }
+);
+
+server.tool(
+  "get-files",
+  "Gets a list of all files that are available.",
+  {
+    dir: z.string().describe("The path or directory to list files from, Unix or Windows style.")
+  },
+  async ({ dir }) => {
+    if (!FileSystem.exists(dir)) {
+      return {
+        content: [
+          {
+            type: "text",
+            text: `Path does not exist: ${dir}`
+          }
+        ]
+      };
+    }
+
+    const items: FolderItem[] = await FileSystem.readFolderItemsAsync(dir);
+    
+    if (items.length === 0) {
+      return {
+        content: [
+          {
+            type: "text",
+            text: "No files found."
+          }
+        ]
+      };
+    }
+
+    const filesString: string = items
+      .filter(file => file.isFile())
+      .map(file => file.name)
+      .join("\n");
+    const foldersString: string = items
+      .filter(file => file.isDirectory())
+      .map(folder => `${folder.name}/`)
+      .join("\n");
+    return {
+      content: [
+        {
+          type: "text",
+          text: `Found files:\n`
+          + filesString
+          + `\nFound directories:\n`
+          + foldersString
         }
       ]
     }
